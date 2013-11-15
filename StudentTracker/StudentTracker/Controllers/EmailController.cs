390c5596d6aa4b6339d1d835f5efc9df8ebd4536
punj-aab <er.singh.lakhwant@live.com>
2013-11-15 07:40:42 -0800

    new

---
+++ StudentTracker/StudentTracker/Controllers/EmailController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentTracker.Controllers
+{
+    public class EmailController : Controller
+    {
+        //
+        // GET: /Email/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+    }
+}
